feb05dc1953011fb6dbf238994d42811605bc9cb
Subodh Dahal <contact.subodhdahal@gmail.com>
2017-11-30 15:10:19 +0100

    Created homepage design

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <Search />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Search from './Search'
+
+  export default {
+    name: 'home',
+
+    components: {
+      Search
+    }
+  }
+</script>
---
+++ src/components/Search.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="col-8 offset-2">
+      <div class="form-group form-inline mt-5">
+        <input type="text" class="form-control p-3 mr-2 search-box" placeholder="Enter location to search">
+
+        <button class="btn btn-primary p-3" type="submit">Search</button>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+    .search-box {
+        width: 85%!important;
+    }
+</style>
