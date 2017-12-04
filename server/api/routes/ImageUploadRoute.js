372f2a7e8b457ccdd3f40877afa9fa7d2bab5339
unknown <karkikiran714@gmail.com>
2017-12-04 12:30:19 +0100

    Update

---
+++ server/api/routes/ImageUploadRoute.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(app) {
+    var imageList = require('../controllers/ImageUploadController');
+
+    // Image Upload  Routes
+    app.route('/images')
+        .get(imageList.list_all_images)
+       .post(imageList.upload_an_image);
+
+
+    app.route('/images/:ImageId')
+        .get(imageList.read_an_image)
+        .put(imageList.update_an_image)
+        .delete(imageList.delete_an_image);
+
+};
