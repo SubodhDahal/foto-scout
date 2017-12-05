39596099fbf0d246fdddd66be3b8aa10f7b03974
Subodh Dahal <contact.subodhdahal@gmail.com>
2017-12-05 23:12:50 +0100

    Added typeahead suggestion for location search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,21 +1,55 @@
 <template>
-    <div class="col-8 offset-2">
-      <div class="form-group form-inline mt-5">
-        <input type="text" class="form-control p-3 mr-2 search-box" placeholder="Enter location to search">
-
-        <button class="btn btn-primary p-3" type="submit">Search</button>
-      </div>
+  <div class="col-8 offset-2">
+    <div class="form-group form-inline mt-5">
+      <vue-google-autocomplete
+        id="map"
+        types=""
+        ref="address"
+        classname="form-control p-3 mr-2 search-box"
+        placeholder="Enter location to search"
+        @placechanged="getAddressData"
+      />
+      <button class="btn btn-primary p-3" type="submit">Search</button>
     </div>
+  </div>
 </template>
 
 <script>
-  export default {
+  import VueGoogleAutocomplete from 'vue-google-autocomplete'
 
+  export default {
+    data () {
+      return {
+        address: {}
+      }
+    },
+
+    components: {
+      VueGoogleAutocomplete
+    },
+
+    mounted () {
+      this.$refs.address.focus()
+    },
+
+    methods: {
+      /**
+      * When the location found
+      * @param {Object} addressData Data of the found location
+      * @param {Object} placeResultData PlaceResult object
+      * @param {String} id Input container ID
+      */
+      getAddressData (addressData, placeResultData, id) {
+        console.log(addressData)
+        console.log(placeResultData)
+        this.address = addressData
+      }
+    }
   }
 </script>
 
 <style>
-    .search-box {
-        width: 85%!important;
-    }
+  .search-box {
+    width: 85%!important;
+  }
 </style>
