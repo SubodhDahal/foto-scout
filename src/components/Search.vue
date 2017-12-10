c8b523c2ad52916865ce42bdf471ff7cd5d845f5
Subodh Dahal <contact.subodhdahal@gmail.com>
2017-12-10 18:58:07 +0100

    Made searched location to showup on the map

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,30 +1,32 @@
 <template>
-  <div class="col-8 offset-2">
-    <div class="form-group form-inline mt-5">
-      <gmap-autocomplete
-        class="form-control p-3 mr-2 search-box"
-        placeholder="Enter location to search"
-        @place_changed="getAddressData"
-      >
-      </gmap-autocomplete>
+  <div class="form-group form-inline mt-5">
+    <gmap-autocomplete
+      class="form-control p-3 mr-2 search-box"
+      placeholder="Enter location to search"
+      @place_changed="getAddressData"
+    >
+    </gmap-autocomplete>
 
-      <button class="btn btn-primary p-3" type="submit">Search</button>
-    </div>
+    <button class="btn btn-primary p-3"
+            @click="getAddressData()"
+            type="submit">
+      Search
+    </button>
   </div>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        address: {}
-      }
-    },
-
     methods: {
       getAddressData (addressData) {
-        console.log(addressData)
-        this.address = addressData
+        // addressData = {
+        //   id: 2,
+        //   lat: 3,
+        //   lng: 5
+        // }
+        this.$store.commit('SET_LOCATION', {
+          location: addressData
+        })
       }
     }
   }
