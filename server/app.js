078d372e52fc1173f550396db3e1402b722b96ac
neekhil <linkinrai@ymail.com>
2017-12-11 18:16:35 +0100

    User authentication api completed

--- server/app.js
+++ server/app.js
@@ -3,20 +3,29 @@ var express = require('express'),
     app = express(),
     mongoose = require('mongoose'),
     User = require('./api/models/userModel'), //created model loading here
+    jwt = require("jsonwebtoken"),
     bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/FotoScoudDB');
+mongoose.connect('mongodb://localhost/FotoScoutDB');
 
 var db = mongoose.connection;
 
-app.use(stormpath.init(app, {
-  // Optional configuration, see documentation
-}));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(function(req, res, next){
+  if(req.headers && req.headers.authorization && req.headers.authorization.split('')[0] === 'JWT'){
+    jwt.verify(req.headers.authorization.split('')[1], 'RESTFULAPIS', function(err, decode){
+      req.user = decode;
+      next();
+    });
+  }else{
+    req.user = undefined;
+    next();
+  }
+});
 
 
 var routes = require('./api/routes/userRoute'); //importing route
