aa751eed2869f7f988411443a61303a4d11d7031
Nischal Rai <nischalpilmong@yahoo.com>
2017-12-14 10:57:54 +0100

    Search Results Added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,18 +2,26 @@
   <div class="container">
     <div class="row">
       <Search />
+      <
+    </div>
+
+    <div class="row">
+      <SearchResults/>
     </div>
   </div>
 </template>
 
 <script>
   import Search from './Search'
+  import SearchResults from './SearchResults'
 
   export default {
     name: 'home',
 
     components: {
-      Search
+      Search,
+      SearchResults
+
     }
   }
 </script>
