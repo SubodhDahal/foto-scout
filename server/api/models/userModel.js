815c97894c7ff2599c19092101153d4267c0d355
Neekhil Rai <linkinrai@ymail.com>
2017-12-29 19:06:39 +0100

    authentication and validation added

--- server/api/models/userModel.js
+++ server/api/models/userModel.js
@@ -1,26 +1,100 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
-    bcrypt = require('bcrypt');
+    bcrypt = require('bcrypt'),
+    jwt = require('jsonwebtoken');
+
+var userProfileSchema = new Schema({
+    about_me:{type: String, default: "None"},
+    profile_pic:{type: String, default: "None"}
+});
 
 var UserSchema = new Schema({
     firstname:{
-        type: String
+      type: String,
+      require: true,
     },
-    lastname: {
-        type: String
-    },
+    lastname:{type: String},
     email:{
-        type: String
+      type: String,
+      require: true,
+      unique: true
     },
     passcode:{
-        type: String
-    }
+      type: String,
+      required: true,
+      minlength: 6
+    },
+    user_profile:[userProfileSchema],
+    tokens: [{
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }]
 });
 
-  UserSchema.methods.comparePassword = function (password) {
-  return bcrypt.compareSync(password, this.passcode);
-}
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'yomantameronepaliho').toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
+UserSchema.statics.findByCredentials = function (email, passcode) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(passcode, user.passcode, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'yomantameronepaliho');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
 
 module.exports = mongoose.model('User', UserSchema);
-
-
--- server/app.js
+++ server/app.js
@@ -4,9 +4,9 @@ var express = require('express'),
     mongoose = require('mongoose'),
     User = require('./api/models/userModel'), //created model loading here
     jwt = require("jsonwebtoken"),
-    Image = require('./api/models/ImageModel'),  
+    Image = require('./api/models/ImageModel'),
     bodyParser = require('body-parser'),
-    path=require('path'),
+    path = require('path'),
     router = express.Router(),
     multer = require('multer');
 
@@ -19,22 +19,14 @@ var db = mongoose.connection;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next){
-  if(req.headers && req.headers.authorization && req.headers.authorization.split('')[0] === 'JWT'){
-    jwt.verify(req.headers.authorization.split('')[1], 'RESTFULAPIS', function(err, decode){
-      req.user = decode;
-      next();
-    });
-  }else{
-    req.user = undefined;
-    next();
-  }
-});
 
 var routes = require('./api/routes/ImageRoute'); //importing route
 var routes1 = require('./api/routes/userRoute');
+
 routes(app);
 routes1(app);
+
+
 //app.use('/', routes);
 app.listen(port);
 console.log('Scout RESTful API server started on: ' + port);
