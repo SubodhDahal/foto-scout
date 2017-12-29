0b4b55fc89d49de4bb1f2a7400af0cfa31b6f17a
indra <endra.giri90@gmail.com>
2017-12-29 12:45:14 +0100

    Created group

--- server/api/routes/groupRoute.js
+++ server/api/routes/groupRoute.js
@@ -4,4 +4,15 @@ module.exports = function(app) {
   var group = require('../controllers/groupController');
   app.route('/group')
     .post(group.create_a_group);
-};
+
+  app.route('/group/:id')
+
+    .get(group.read_group)
+}
+
+
+   /* .post(group.update_group)
+    .delete (group.delete_group);
+   .add(group.add_user)*/
+
+
