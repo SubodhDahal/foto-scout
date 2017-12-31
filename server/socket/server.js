6853f9bd1caa2158e1d58283e362f89086f0afb8
ParajMhrjn <33930645+ParajMhrjn@users.noreply.github.com>
2017-12-31 04:08:04 +0100

    changes some socket.io features to send messages

--- server/socket/server.js
+++ server/socket/server.js
@@ -4,26 +4,33 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-
+users = [];
+connections = [];
+//server connection
 server.listen(process.env.PORT || 3000);
 console.log ('Server is running.....');
 
+//routing
 app.get ('/', function(req, res){
   res.sendFile(__dirname + '/chatroom.html');
 });
 
-//to connect socket.io
+//to connect user
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  connections.push(socket);
+  console.log('connected: %s user is connected', connections.length);
+  //to disconnect user
+  socket.on('disconnect',function(data){
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('disconnected: %s user is disconnected', connections.length);
   });
-});
 //to boardcast message
-io.emit('some event', { for: 'everyone' });
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('send message', function(data){
+    console.log(data);
+    io.sockets.emit('new message', {msg: data});
   });
 });
+
+
+
 
