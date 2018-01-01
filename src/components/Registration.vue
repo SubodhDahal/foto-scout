7afd4da65b75858a44d001ac9f5f53eb12b56fbd
Hem Joshi <nirvanacobain.hem@gmail.com>
2018-01-01 23:54:17 +0100

    Linked Registration UI with Server

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -10,14 +10,35 @@
         </div>
         <div class="row">
             <div class="col-md-3 field-label-responsive">
-                <label for="name">Name</label>
+                <label for="firstname">First Name</label>
             </div>
             <div class="col-md-6">
                 <div class="form-group">
                     <div class="input-group mb-2 mr-sm-2 mb-sm-0">
                         <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-user"></i></div>
-                        <input type="text" name="name" class="form-control" id="name"
-                               placeholder="John Doe" required autofocus>
+                        <input v-model="firstname" type="text" name="firstname" class="form-control" id="firstname"
+                               placeholder="John" required autofocus>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-control-feedback">
+                        <span class="text-danger align-middle">
+                            <!-- Put first name validation error messages here -->
+                        </span>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 field-label-responsive">
+                <label for="lastname">Last Name</label>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                        <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-user"></i></div>
+                        <input v-model="lastname" type="text" name="lastname" class="form-control" id="lastname"
+                               placeholder="Doe" required autofocus>
                     </div>
                 </div>
             </div>
@@ -37,7 +58,7 @@
                 <div class="form-group">
                     <div class="input-group mb-2 mr-sm-2 mb-sm-0">
                         <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-at"></i></div>
-                        <input type="text" name="email" class="form-control" id="email"
+                        <input v-model="email" type="text" name="email" class="form-control" id="email"
                                placeholder="you@example.com" required autofocus>
                     </div>
                 </div>
@@ -58,16 +79,13 @@
                 <div class="form-group has-danger">
                     <div class="input-group mb-2 mr-sm-2 mb-sm-0">
                         <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-key"></i></div>
-                        <input type="password" name="password" class="form-control" id="password"
+                        <input v-model="password" type="password" name="password" class="form-control" id="password"
                                placeholder="Password" required>
                     </div>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="form-control-feedback">
-                        <span class="text-danger align-middle">
-                            <i class="fa fa-close"></i>
-                        </span>
                 </div>
             </div>
         </div>
@@ -96,13 +114,35 @@
     </form>
 </div>
 </template>
-
 <script>
+import axios from 'axios'
 export default {
+  data () {
+    return {
+      isUserRegistered: false,
+      firstname: '',
+      lastname: '',
+      email: '',
+      passcode: ''
+    }
+  },
+  methods: {
+    onRegister () {
+      let registerData = new FormData()
+      registerData.append('firstname', this.firstname)
+      registerData.append('lastname', this.lastname)
+      registerData.append('email', this.email)
+      registerData.append('passcode', this.passcode)
+      axios.post('http://localhost:3000/register', registerData)
+        .then((response) => {
+          if (response.data.success === 'true') {
+            this.isUserRegistered = true
+          }
+        })
+    }
+  }}
 
-}
 </script>
-
 <style>
   @media(min-width: 768px) {
   .field-label-responsive {
@@ -111,3 +151,4 @@ export default {
   }
 }
 </style>
+
