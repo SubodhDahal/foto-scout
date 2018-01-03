d2d5cadde8ceb3a3597e040e3f316fc48a4e463c
Nischal Rai <nischalpilmong@yahoo.com>
2018-01-03 14:01:33 +0100

    changed the placeholder title of firstname and lastname

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -26,7 +26,7 @@
                     <div class="input-group mb-2 mr-sm-2 mb-sm-0">
                         <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-user"></i></div>
                         <input v-model="firstname" type="text" name="firstname" class="form-control" id="firstname"
-                               placeholder="John" required autofocus>
+                               placeholder="Enter first name" required autofocus>
                     </div>
                 </div>
             </div>
@@ -47,7 +47,7 @@
                     <div class="input-group mb-2 mr-sm-2 mb-sm-0">
                         <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-user"></i></div>
                         <input v-model="lastname" type="text" name="lastname" class="form-control" id="lastname"
-                               placeholder="Doe" required autofocus>
+                               placeholder="Enter your lastname" required autofocus>
                     </div>
                 </div>
             </div>
