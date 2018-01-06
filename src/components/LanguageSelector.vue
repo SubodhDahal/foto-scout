4b88e5b9fceb4593d1ade096af7a994bf5a1d717
Subodh Dahal <contact.subodhdahal@gmail.com>
2018-01-07 00:44:01 +0100

    Navigation bar design change

--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="mt-3 float-right">
     <Language name="English" locale="en" flag="UK" />
     <Language name="Deutsch" locale="de" flag="DE" />
   </div>
--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -1,35 +1,52 @@
 <template>
-  <div class="container" v-if="isUserLoggedIn">
-    <b-navbar toggleable="md" type="light" variant="info">
+  <div class="container">
+    <b-navbar toggleable="md" type="light" class="py-3 main-nav">
+      <template v-if="isUserLoggedIn">
+        <ul class="nav navbar-nav">
+          <li class="nav-item mr-4">
+            <router-link :to="{name:'Home'}">
+              <i class="fa fa-home" aria-hidden="true"></i>
+            </router-link>
+          </li>
+          <li class="nav-item mr-4">
+            <router-link :to="{name:'UploadImage'}">
+              Upload Image
+            </router-link>
+          </li>
 
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item mr-4">
-          <router-link :to="{name:'UploadImage'}">
-            Upload Image
-          </router-link>
-        </li>
+          <li class="nav-item mr-4">
+            <router-link :to="{name:'UserImages'}">
+              My Images
+            </router-link>
+          </li>
 
-        <li class="nav-item mr-4">
-          <router-link :to="{name:'UserImages'}">
-            My Images
-          </router-link>
-        </li>
+          <li class="nav-item mr-4">
+            <router-link :to="{name:'DisplayGroup'}">
+              Groups
+            </router-link>
+          </li>
+        </ul>
 
-        <li class="nav-item mr-4">
-          <router-link :to="{name:'DisplayGroup'}">
-            Groups
-          </router-link>
-        </li>
-      </ul>
+        <ul class="nav navbar-nav justify-content-end">
+          <li class="nav-item mr-4">
+            Welcome {{ userDetails.firstname }}
+          </li>
+          <li class="nav-item mr-4">
+            <a href="#" @click.prevent="logout">Logout</a>
+          </li>
+        </ul>
+      </template>
 
-      <ul class="navbar-nav mr-auto float-right">
-        <li class="nav-item mr-4">
-          Welcome {{ userDetails.firstname }}
-        </li>
-        <li class="nav-item mr-4">
-          <a href="#" @click.prevent="logout">Logout</a>
-        </li>
-      </ul>
+      <template v-if="!isUserLoggedIn">
+        <ul class="nav navbar-nav justify-content-end">
+          <li class="nav-item mr-4">
+            <router-link :to="{name:'Registration'}">{{ $t('register') }}</router-link>
+          </li>
+          <li class="nav-item mr-4">
+            <router-link :to="{name:'UserLogin'}">{{ $t('login') }}</router-link>
+          </li>
+        </ul>
+      </template>
     </b-navbar>
   </div>
 </template>
@@ -57,6 +74,10 @@
 </script>
 
 <style>
+  .main-nav {
+    border-radius: 5px;
+    background-color: #dd823a !important;
+  }
   .navbar-nav, .navbar-nav a {
     color: #fff;
   }
