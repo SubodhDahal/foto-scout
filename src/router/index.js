5fee793642e39a3762a5dc0a7e313e2042274f61
Hem <nirvanacobain.hem@gmail.com>
2018-01-07 12:29:49 +0100

    Organized user components in a different folder

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import Registration from '@/components/Registration.vue'
 import SearchResults from '@/components/SearchResults.vue'
 import CarouselImage from '@/components/CarouselImage.vue'
 import UploadImage from '@/components/UploadImage'
 import DisplayGroup from '@/components/DisplayGroup'
 import CreateGroup from '@/components/CreateGroup'
 import UpdateGroup from '@/components/UpdateGroup'
-import UserLogin from '@/components/UserLogin'
+import Registration from '@/components/User/Registration.vue'
+import UserLogin from '@/components/User/Login'
 import EditImage from '@/components/EditImage'
 import UserImages from '@/components/UserImages'
 
@@ -25,16 +25,21 @@ export default new Router({
       path:'/register',
        name:'Registration',
        component:Registration
-     },
+    },
     {
-     path:'/search-results',
-      name:'SearchResults',
-      component:SearchResults
+      path:'/login',
+      name:'UserLogin',
+      component:UserLogin
     },
     {
       path:'/profile/images',
       name:'UserImages',
       component:UserImages
+    },
+    {
+     path:'/search-results',
+      name:'SearchResults',
+      component:SearchResults
     },
     {
       path:'/carousel-image/:id/:type',
@@ -60,11 +65,6 @@ export default new Router({
       path:'/update-group/:id',
       name:'EditGroup',
       component:UpdateGroup
-    },
-    {
-      path:'/login',
-      name:'UserLogin',
-      component:UserLogin
     },
     {
       path:'/edit-image/:id',
